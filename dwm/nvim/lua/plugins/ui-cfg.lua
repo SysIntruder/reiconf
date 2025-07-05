3c7a8aacfdc3e211c6ec53e4a197668a8da04ec1
rei <raihanryuichi@gmail.com>
2025-07-05 19:36:45 +0700

    update dwm color

--- dwm/nvim/lua/plugins/ui-cfg.lua
+++ dwm/nvim/lua/plugins/ui-cfg.lua
@@ -5,6 +5,16 @@ return {
 		config = function()
 			require("themer").setup({
 				colorscheme = "dracula",
+				remaps = {
+					highlights = {
+						dracula = {
+							base = {
+								StatusLine = { bg = "#282a36", fg = "#f8f8f2" },
+								StatusLineNC = { bg = "#282a36", fg = "#6272a4" },
+							},
+						},
+					},
+				},
 			})
 		end,
 	},
