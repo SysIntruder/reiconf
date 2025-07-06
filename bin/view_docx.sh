4e00b932a74211c8f393ca265a2294df93eb9b22
rei <raihanryuichi@gmail.com>
2025-07-06 11:54:19 +0700

    update

--- bin/view_docx.sh
+++ bin/view_docx.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if ! command -v docx2md &> /dev/null
+if ! command -v docx2txt &> /dev/null
 then
-  echo "docx2md not found"
+  echo "docx2txt not found"
   exit 1
 fi
 
 FILENAME="$1"
 
-docx2md "$FILENAME" | less
+docx2txt < "$FILENAME" | less
